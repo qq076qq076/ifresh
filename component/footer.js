7fbfd0762360a514b35892039034d0c5d23e15dc
qq076qq076 <qq076qq076@gmail.com>
2018-06-28 23:45:00 +0800

    first commit

---
+++ component/footer.js
@@ -0,0 +1,25 @@
+Vue.component('footer-component',
+ {
+    template: `
+        <footer>
+            <nav>
+                <a class="item">
+                    <img src="./image/footer/search.png" />
+                   <p>搜尋</p>
+                </a>
+                <a class="item">
+                    <img src="./image/footer/member.png" />
+                    <p>會員</p>
+                </a>
+                <a class="item">
+                    <img src="./image/footer/favorite.png" />
+                    <p>收藏</p>
+                </a>
+                <a class="item">
+                    <img src="./image/footer/shopcard.png" />
+                    <p>結帳</p>
+                </a>
+            </nav>
+        </footer>
+    `,
+});
---
+++ component/header.js
@@ -0,0 +1,13 @@
+Vue.component('header-component', {
+    template: `
+        <header>
+            <i class="menu"/>
+            <div class="logo-space">
+                <img src="./image/logo.png" />
+            </div>
+            <i class="footerint">
+                <img src="./image/footprint.png" width="24px" />
+            </i>
+        </header>
+    `,
+});
